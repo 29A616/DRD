ed7649e15f166cc702f875a33dafea34c8b155dd
29A616 <gnx616@gmail.com>
2024-12-08 15:50:25 -0400

    v0.01

--- DRD/settings.py
+++ DRD/settings.py
@@ -13,21 +13,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$u2@*7wjwt6r@w@f(cqt7*&km!v9$j#y6zf-nyqpxz&8f%%l^e'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY', 'django-insecure-$u2@*7wjwt6r@w@f(cqt7*&km!v9$j#y6zf-nyqpxz&8f%%l^e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['railway.app']
 
 # Application definition
 
@@ -107,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Etc/GMT+4'
 
 USE_I18N = True
 
@@ -118,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
